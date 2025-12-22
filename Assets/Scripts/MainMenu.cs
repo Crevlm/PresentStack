2e73cac04caa96d75650b73d3b591d066393800f
oblek1 <escoto111@gmail.com>
2025-12-22 11:14:49 -0600

    main menu UI sfx

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using FMOD.Studio;
+using FMODUnity;
+using FMOD;
+using System;
 public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private string gameSceneName = "GameScene";
 
+    private const string ConfirmEventPath = "event:/SFX/UI_Confirm";
+    private const string ReturnEventPath = "event:/SFX/UI_Select";
+
+  
 
     public void StartGame()
     {
+        RuntimeManager.PlayOneShot(ConfirmEventPath);
         SceneManager.LoadScene(gameSceneName);
     }
 
     public void QuitGame()
     {
+        RuntimeManager.PlayOneShot(ReturnEventPath);
         Application.Quit();
 #if UNITY_EDITOR
         // Allows quit to work in the Editor for testing
